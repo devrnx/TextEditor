54693af21732f7bd3c22d92e95769a4a6862b91f
Rennan Alves <rennan.alves@take.net>
2022-11-21 12:17:32 -0300

    Create functions

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApp
 {
@@ -6,7 +7,83 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("");
+            Console.Write("Escolha uma opção: ");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: Environment.Exit(0);
+                    break;
+                case 1:
+                    Open();
+                    break;
+                case 2:
+                    Edit();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void Open() 
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo? ");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Edit()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo: (ESC para sair)");
+            Console.WriteLine("----------------------------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape); // enquanto o usuário não clicar no ESC
+
+            Save(text);
+        }
+
+        static void Save(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo? ");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path)) 
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.WriteLine("");
+            Menu();
         }
     }
 }
